a5790d6f7674d5e5701c5b831a5a59208595180a
= <aranlong@live.co.uk>
2016-10-01 19:06:21 +0100

    frontend webcam work

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,50 @@
 $(function() {
-  var $inputMessage = $('.inputMessage'); // Input message input box
-  var $window = $(window);
+  Webcam.set({
+        width: 320,
+        height: 240,
+        dest_width: 640,
+        dest_height: 480,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
 
-  console.log('connecting');
-  var socket = io("http://aranlong.co.uk:3005");
-  //socket.emit('add user', username);
+    Webcam.attach('#oldcamera');
 
-  function sendMessage () {
-    var message = $inputMessage.val();
-    socket.emit('message', message);
-  }
+    var $inputMessage = $('.inputMessage'); // Input message input box
+    var $window = $(window);
 
+    console.log('connecting');
+    var socket = io("http://aranlong.co.uk:3004");
+    //socket.emit('add user', username);
 
-  $window.keydown(function (event) {
-    if(event.which == 13) {
+    socket.on('message', function(data) {
+      console.log('received message');
+      document.getElementById('camera').innerHTML = '<img src="' + data + '" />';
+    });
 
-      sendMessage();
+    function sendMessage() {
+        var message = $inputMessage.val();
+        socket.emit('message', message);
+        console.log('message sent');
     }
-  }
+
+
+    $window.keydown(function(event) {
+        if (event.which == 13) {
+            console.log('enter hit');
+
+            sendMessage();
+        }
+    });
+
+
+
+    function snapshot() {
+        Webcam.snap(function(data_uri) {
+            socket.emit('message', data_uri);
+        });
+    }
+
+    setInterval(snapshot, 1000);
+
 });
